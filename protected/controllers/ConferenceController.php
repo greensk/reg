2d50adff9719977f0411ca31b9b7bb49afbda827
Grigory Kosheev <dev@green.novosibirsk.ru>
2013-12-05 21:13:39 +0700

    Commented conference view action and view

--- protected/controllers/ConferenceController.php
+++ protected/controllers/ConferenceController.php
@@ -2,11 +2,34 @@
 // protected/controllers/ConferenceController.php
 class ConferenceController extends Controller {
 	
+	/**
+	 * Страница конференции.
+	 * 
+	 * @param int $id
+	 * 
+	 */
 	public function actionView($id)
 	{
+		/*
+		 * Получение модели конкретной конференции 
+		 * Conference::model — создание пустой модели конференции (не
+		 * соответствующей какой-либо конкретной сущности).
+		 * findByPk — нахождение записи с заданным ключем (Primary key).
+		 */
 		$conference = Conference::model()->findByPk($id);
-		if (!$conference)
+		
+		/*
+		 * Если соответствующей записи не существует, $conference
+		 * принимает значение null. В этом случае нужно выдать 
+		 * пользователю ошибку HTTP 404.
+		 */
+		if ($conference === null)
 			throw new CHttpException(404);
+			
+		/*
+		 * Отображаем представление "view" для данного контроллера.
+		 * Передаем ему в качестве параметра модель конференции.
+		 */
 		$this->render('view', array('model' => $conference));
 	}
 	
