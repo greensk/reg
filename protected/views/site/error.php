6d3ab46f1abd2fe32ff0bd1ca8869c0951975372
Grigory Kosheev <dev@green.novosibirsk.ru>
2013-12-10 00:20:38 +0700

    Legacy code removed

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -2,13 +2,7 @@
 /* @var $this SiteController */
 /* @var $error array */
 
-$this->pageTitle=Yii::app()->name . ' - Error';
-$this->breadcrumbs=array(
-	'Error',
-);
-?>
-
-<h2>Error <?php echo $code; ?></h2>
+<h2>Ошибка <?php echo $code; ?></h2>
 
 <div class="error">
 <?php echo CHtml::encode($message); ?>
